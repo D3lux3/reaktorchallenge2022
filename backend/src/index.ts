0f80fd5625fdd45ffab61610638cd4a8092fb4f4
D3lux3 <38981163+D3lux3@users.noreply.github.com>
2022-01-20 21:30:13 +0200

    fixed bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,7 +28,7 @@ app.get('/', (_req, res) => {
 
 const start = async () => {
 
-    app.listen(3002, () => {
+    app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
     const ws = new WebSocket('ws://bad-api-assignment.reaktor.com/rps/live');
