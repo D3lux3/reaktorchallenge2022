d55b720237cab7a1e1c92d4d2036c2ade6699f90
D3lux3 <38981163+D3lux3@users.noreply.github.com>
2022-01-20 21:40:47 +0200

    heroku please

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,18 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import axios from 'axios';
 import WebSocket from 'ws';
-import express from 'express';
 import { dataSchema, DataType } from './types';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3002;
-
+const ws = new WebSocket('ws://bad-api-assignment.reaktor.com/rps/live');
+ws.on('message', async (data) => {
+    const gameData = await dataSchema.validate(JSON.parse(data.toString()));
+    gameData.t = Date.now();
+    if (gameData.type === 'GAME_RESULT') {
+        await addGameResultToHistory(gameData);
+    }
+});
 
 const addGameResultToHistory = async (data: DataType) => {
     const historyApiURI = process.env.HISTORY_API_URI || 'http://localhost:3001/api/history';
@@ -22,25 +26,10 @@ const addGameResultToHistory = async (data: DataType) => {
 };
 
 //Only implemented express, so this heroku app can be waken up.
-app.get('/', (_req, res) => {
-    res.status(200).json({ message: 'i am awake' });
-});
-
-const start = async () => {
-
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-    const ws = new WebSocket('ws://bad-api-assignment.reaktor.com/rps/live');
-    ws.on('message', async (data) => {
-        const gameData = await dataSchema.validate(JSON.parse(data.toString()));
-        gameData.t = Date.now();
-        if (gameData.type === 'GAME_RESULT') {
-            await addGameResultToHistory(gameData);
-        }
-    });
-};
 
 
-void start();
+
+
+
+
 
