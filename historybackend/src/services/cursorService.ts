fcf727e958570d1d453191444a31f3f1bea8ffba
D3lux3 <dani.haapaniemi@hotmail.com>
2022-01-21 21:15:18 +0100

    Added comments to functions

--- historybackend/src/services/cursorService.ts
+++ historybackend/src/services/cursorService.ts
@@ -12,20 +12,28 @@ interface CountType {
     count: number
 }
 
+/**
+ * Returns true if all the historical game result has been parsed.
+ * @returns 
+ */
 export const isHistoryParsed = async () => {
     try {
-        const lastCount = await sequelize.query(`
+        const lastCounter = await sequelize.query(`
         SELECT COUNT(*)
         FROM cursors C
         WHERE C.last = true;
         `, { type: QueryTypes.SELECT });
-        return (lastCount as CountType[])[0].count >= 1 || false;
+        return (lastCounter as CountType[])[0].count >= 1 || false;
      } catch(error) {
          console.log(error);
      }
      return false;
 };
 
+/**
+ * Gets the latest cursor that has been parsed, so the parsing can continue from the latest cursor.
+ * @returns 
+ */
 export const getLatestCursor = async () => {
     try {
        const latestCursor =  await sequelize.query(`
@@ -40,6 +48,11 @@ export const getLatestCursor = async () => {
     return "/rps/history";
 };
 
+/**
+ * Adds single cursor to the database.
+ * @param cursorObj 
+ * @param t 
+ */
 export const addCursorToDB = async (cursorObj: CursorCreationAttributes, t: Transaction) => {
     await Cursor.upsert(cursorObj, {transaction: t});
 };
--- historybackend/src/services/historyService.ts
+++ historybackend/src/services/historyService.ts
@@ -8,9 +8,10 @@ import { addCursorToDB, getLatestCursor, isHistoryParsed } from './cursorService
 
 /**
  * Parses through the game history and saves it to a database.
+ * Starting the project will parse new unparsed game history, if prior game history is already parsed.
  * @param url Bad-api url.
  * @param currentCursor Next cursor to be concatenated with url
- * @param parsedCursors All the cursors that has been parsed before.
+ * @param parsedCursors All the cursors that has been parsed for game data prior.
  * @returns 
  */
 export const parseHistoricalGameResults = async (url: string, currentCursor: string, parsedCursors: string[]) => {
@@ -35,6 +36,12 @@ export const parseHistoricalGameResults = async (url: string, currentCursor: str
     }
 };
 
+/**
+ * Adds one cursors worth game history gathered from the api to database.
+ * This function is used by the websocket backend mainly.
+ * @param data Data from the Bad-Api
+ * @returns
+ */
 export const addOneGameResultToDB = async (data: unknown) => {
     try {
         const validated = await dataSchema.validate(data);
@@ -48,7 +55,9 @@ export const addOneGameResultToDB = async (data: unknown) => {
 };
 
 /**
- * This function deletes history data, when reaches heroku limit (10000) on database row amount.
+ * Deletes history data, when reaches heroku limit (10000) on database row amount.
+ * This function is implented only because Heroku has a database row limit.
+ *
  */
 const deleteGameResults = async () => {
     const rowsLeft = await getRowLimit();
@@ -60,16 +69,25 @@ const deleteGameResults = async () => {
     }
 };
 
+/**
+ * Returns the amount of rows in the database filled with game history data.
+ * @returns 
+ */
 const amountOfHistoricalGameResults = async () => {
     return await GameResult.count();
 };
 
-const handleTransactionToDB = async (data: ResponseDataType[], c: CursorCreationAttributes) => {
+/**
+ * Creates a transaction for database that handles saving cursor and the game history data supplied.
+ * @param data 
+ * @param cursorToBeSaved 
+ */
+const handleTransactionToDB = async (data: ResponseDataType[], cursorToBeSaved: CursorCreationAttributes) => {
     let transaction;
     try {
         transaction = await sequelize.transaction();
         await addGameResultsToDB(data, transaction);
-        await addCursorToDB(c, transaction);
+        await addCursorToDB(cursorToBeSaved, transaction);
         await transaction.commit();
     } catch (error) {
         if (transaction) {
@@ -78,7 +96,11 @@ const handleTransactionToDB = async (data: ResponseDataType[], c: CursorCreation
         console.log(error);
     }
 };
-
+/**
+ * Adds all of the game history data in bulk to database.
+ * @param data 
+ * @param t 
+ */
 const addGameResultsToDB = async (data: ResponseDataType[], t: Transaction) => {
     const newData = data.map((game) => (
         dataToDatabaseFormat(game)
@@ -86,7 +108,10 @@ const addGameResultsToDB = async (data: ResponseDataType[], t: Transaction) => {
     await GameResult.bulkCreate(newData, { transaction: t, ignoreDuplicates: true });
 };
 
-
+/**
+ * Gets all of the game history from the database.
+ * @returns Game history
+ */
 export const getAllHistory = async () => {
     const history = await GameResult.findAll();
     return history;
